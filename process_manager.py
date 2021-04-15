6d0648911c50e55f110a04de0deab86db3058db2
JMJ <875694683@qq.com>
2021-04-15 23:45:59 +0800

    completed three functions

--- process_manager.py
+++ process_manager.py
@@ -3,6 +3,7 @@
 
 import sys
 import time
+import copy
 import threading
 
 
@@ -11,13 +12,16 @@ class PCB(object):
     Atrributes:
         pid,pname,parentid,priority,create_time,status,
         tasklist: 该进程待执行的任务
+        msize: 该进程所占用的内存大小
     """
-    def __init__(self, pid, pname, parentid, priority, create_time, content):
+    def __init__(self, pid, pname, priority, content, msize):
         self.pid = pid
         self.pname = pname
-        self.parentid = parentid
-        self.priority = priority
-        self.create_time = create_time
+        self.priority = priority  # 0 is higher than 1
+        self.msize = msize
+        self.parent_id = -1
+        self.child_num = 0
+        self.create_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
         self.status = 'ready'
         self.tasklist = []
         for task in content:
@@ -32,55 +36,100 @@ class ProcessManager(object):
     def __init__(self, memory_manager, time_slot=1, priority=True):
         """ 
         Args:
-            pidum:进程数量
+            pid_no: 下个进程的序号
             pcblist: 被管理进程的PCB
             ready_quene: 就绪队列，分为三个优先级
             waiting_quene:等待队列
             p_running：正在运行的进程，同一时间只有一个
             memory_manager: 每个进程所对应的内存及其管理器
+            mem_of_pid：每个进程所对应的内存号
         """
-        self.pidnum = 0
+        self.pid_no = 0
         self.pcblist = []
-        self.ready_quene = [[]for i in range(3)]
+        self.ready_quene = [[] for i in range(3)]
         self.waiting_quene = []
         self.p_running = -1
         self.memory_manager=memory_manager
         self.time_slot=time_slot
         self.priority=priority
+        self.mem_of_pid = {}
 
-    def create(self, command):
-        """ 由命令创建进程 """
-        pass
+    def create(self, exefile):
+        """ 打开程序文件创建进程 """
+        if exefile['type'] != 'exec':
+            self.error_handler('exec')
+        else:
+            mem_no = self.memory_manager.alloc(self.pid_no, int(exefile['size'])) 
+            if mem_no == -1:
+                self.error_handler('mem')
+            else:
+                pcb = PCB(self.pid_no, exefile['name'], exefile['priority'], exefile['content'], int(exefile['size']))
+                self.pcblist.append(pcb)
+                self.ready_quene[exefile['priority']].append(pcb.pid)
+                self.mem_of_pid[pcb.pid] = mem_no
+                self.pid_no += 1
+                print(f'[pid {pcb.pid}] process created successfully')
 
     def fork(self):
         """ 创建子进程 """
-        pass
+        child_msize = self.pcblist[self.p_running].msize
+        mem_no = self.memory_manager.alloc(self.pid_no, child_msize) 
+        if mem_no == -1:
+            self.error_handler('mem')
+        else:
+            # 初始化子进程pcb
+            child_pcb = copy.deepcopy(self.pcblist[self.p_running]) 
+            child_pcb.pid = self.pid_no
+            child_pcb.parent_id = self.p_running
+            child_pcb.create_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
 
-    def getPstatus(self):
+            self.pcblist.append(child_pcb)
+            self.ready_quene[child_pcb.priority].append(child_pcb.pid)
+            self.pid_no += 1
+            self.pcblist[self.p_running].child_num += 1
+            print(f'[pid {self.pid_no}] process forked successfully by [pid {self.p_running}]' )
+
+
+
+    def get_status(self, pid):
         """ 获取进程当前状态 """
-        pass
+        return self.pcblist[pid].status
 
     def dispatch(self):
         """ 调度进程，ready->running """
-        pass
+        for level in range(0, len(self.ready_quene)):
+            if self.ready_quene[level]:
+                self.p_running = self.ready_quene[level][0]
+                self.ready_quene[level].pop(0)
+                self.pcblist[self.p_running].status = 'running'
+        self.p_running = -1  # ready队列为空，无进程可运行
 
     def timeout(self):
-        """ 时间片用尽，running->ready """
-        pass
+        """ 时间片用尽，running->ready/terminate """
+        
 
-    def ioWait(self, pid):
+    def io_wait(self, pid):
         """ 等待io事件，running->waiting """
         pass
     
-    def ioCompletion(self, pid):
+    def io_completion(self, pid):
         """ io完成，wating->ready """
         pass
 
-    def killProcess(self, pid):
+    def kill_process(self, pid):
         pass
 
-    def startManager(self):
+    def start_manager(self):
         """ 主逻辑，启动模块并运行 """
+
+    def error_handler(self, type):
+        if type == 'mem':
+            print("Failed to create new process: No enough memory.")
+        elif type== 'exec':
+            print('Failed to exucute: Not an executable file.')
+        elif type == 'kill':
+            print('Failed to kill process: No such process.')
+
 
 if __name__ == '__main__':
     pm = ProcessManager()
