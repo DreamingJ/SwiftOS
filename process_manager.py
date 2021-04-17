7e9cf5409abb764224641ff45293f04d7314e54d
JMJ <875694683@qq.com>
2021-04-17 22:32:36 +0800

    增加类依赖config，完善start函数

--- process_manager.py
+++ process_manager.py
@@ -5,13 +5,8 @@ import sys
 import time
 import copy
 import threading
-
-""" 几个可能问题：
-TODO
-        mem_manager没被import就直接用吗？
-        .pc的逻辑可以被优化吗？
-        timeout函数逻辑放哪里
-"""
+from config import *
+from process_manager import MemoryManager
 
 
 class PCB(object):
@@ -35,14 +30,17 @@ class PCB(object):
         for task in content:
             info = str.split(task)  
             if len(info) > 1:
-                info[1] = int(info[1])
+                if info[0] == 'cpu':
+                    info[1] = float(info[1])
+                else:
+                    info[1] = int(info[1])
             self.tasklist.append(info)  # example: [[printer, 18], [cpu, 170]]
         self.current_task = -1
 
 
 class ProcessManager(object):
     """ Provide functions to manage process"""
-    def __init__(self, memory_manager, time_slot=1, priority=True):
+    def __init__(self, memory_manager, priority=True):
         """ 
         Args:
             pid_no: 下个进程的序号
@@ -59,26 +57,25 @@ class ProcessManager(object):
         self.waiting_quene = []
         self.p_running = None
         self.memory_manager=memory_manager
-        self.time_slot=time_slot
+        self.time_slot=time_slot_conf
         self.priority=priority
         self.mem_of_pid = {}
 
     def create(self, exefile):
         """ 打开程序文件创建进程 """
-        if exefile['type'] != 'exec':
+        if exefile['type'][0] != 'e':
             self.error_handler('exec')
         else:
             mem_no = self.memory_manager.alloc(self.pid_no, int(exefile['size'])) 
             if mem_no == -1:
                 self.error_handler('mem')
             else:
-                """ TODO 是否有breakbug，对比下 """
                 pcb = PCB(self.pid_no, exefile['name'], exefile['priority'], exefile['content'], int(exefile['size']))
                 self.pcblist.append(pcb)
                 self.ready_quene[exefile['priority']].append(pcb.pid)
                 self.mem_of_pid[pcb.pid] = mem_no
                 self.pid_no += 1
-                print(f'[pid {pcb.pid}] process created successfully')
+                print(f'[pid {pcb.pid}] process created successfully.')
 
     def fork(self):
         """ 创建子进程 """
@@ -96,7 +93,7 @@ class ProcessManager(object):
             self.pcblist.append(child_pcb)
             self.ready_quene[child_pcb.priority].append(child_pcb.pid)
             self.pid_no += 1
-            self.p_running.child_pid_list
+            self.p_running.child_pid_list.append(child_pcb)
             print(f'[pid {self.pid_no}] process forked successfully by [pid {self.p_running.pid}]' )
 
 
@@ -107,6 +104,7 @@ class ProcessManager(object):
 
     def dispatch(self):
         """ 调度进程，ready->running """
+        self.p_running = None
         for level in range(0, len(self.ready_quene)):
             # 就绪队列不为空
             if self.ready_quene[level]:
@@ -119,9 +117,13 @@ class ProcessManager(object):
         """ 时间片用尽，running->ready/terminate """
         if self.p_running:
             # 当前是否执行到进程的最后一条任务
-            if self.p_running.current_task == len(self.p_running.command_quene):
-                terminate()
-            # 进程进入就绪队列继续等待执行
+            if self.is_last_task(self.p_running.pid):
+                if(self.memory_manager.free(self.p_running.pid)):
+                    self.p_running.status = 'terminated'
+                    print(f'[Pid #{self.p_running.pid}] finished tasks, turn to terminated.')
+                else:
+                    print("Failed to free the memory of [Pid #%d]." % self.p_running.pid)
+            # 不是最后一条，进程进入就绪队列继续等待执行
             else:
                 self.p_running.status = 'ready'
                 level = self.p_running.priority
@@ -139,10 +141,31 @@ class ProcessManager(object):
     def kill_process(self, pid):
         pass
 
+    def is_last_task(self, pid):
+        return self.pcblist[pid].current_task == len(self.pcblist[pid].tasklist)
+
     def start_manager(self):
         """ 主逻辑，启动模块并运行 """
         while True:
             self.dispatch()
+            if self.p_running:
+                # 在一个时间片内：
+                task = self.p_running.tasklist[self.p_running.current_task]
+                if task[0] == 'cpu':
+                    if task[1] > self.time_slot:
+                        time.sleep(self.time_slot)
+                        self.timeout()
+                    else:
+                        time.sleep(task[1])
+                        # 剩余时间继续下一条指令
+
+                # task[0]的值变了没，还能用吗？
+                elif task[0] == 'fork':
+                    pass
+                elif task[0] == 'io':
+                    pass
+
+
 
 
     def error_handler(self, type):
@@ -155,4 +178,7 @@ class ProcessManager(object):
 
 
 if __name__ == '__main__':
-    pm = ProcessManager()
+    memory = MemoryManager(mode=memory_management_mode,page_size=memory_page_size,
+                                                page_number=memory_page_number,
+                                                physical_page=memory_physical_page_number)
+    pm = ProcessManager(memory)
